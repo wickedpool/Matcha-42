5569c8a995aa4277758ae44cc0b720e5262d0924
Wickedpool <thgiraud@student.42.fr>
2017-08-23 19:38:45 +0200

    start check

--- routes/register.js
+++ routes/register.js
@@ -2,6 +2,7 @@ var express = require('express'),
 	router = express.Router();
 
 router.post('/', function(req, res) {
+	if (req.body.login && req.body.name && req.body.lastname && req.body.email && req.body.age && req.body.gender)
 	res.send("ON EST AL !");
 });
 module.exports = router;
