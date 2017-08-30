d7891180077274869781fee14c57bb801401b4cc
Wickedpool <thgiraud@student.42.fr>
2017-08-30 20:26:28 +0200

    all

--- app.js
+++ app.js
@@ -1,69 +1,66 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
+var express = require('express')
+var path = require('path')
+//var favicon = require('serve-favicon')
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var session = require('express-session')
 
-var index = require('./routes/index');
-var register = require('./routes/register');
+var index = require('./routes/index')
+var register = require('./routes/register')
 
-var app = express();
+var app = express()
 
 // view engine setup
-//
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-// uncomment after placing your favicon in /public app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//
-//app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(session({
-	secret: "welfwibfaiwc",
+	secret: "Zhowh>fpui901884384jdow<kkd",
 	resave: false,
 	saveUninitialized: true,
 	cookie: { secure: false }
-}));
+}))
 
 app.use(function (req, res, next) {
 	if (req.session) {
 		if (req.session.error) {
-			console.log(req.session.error);
+			console.log(req.session.error)
 			res.locals.error = req.session.error
 			req.session.error = undefined
 		}
 	}
 	next()
-});
+})
 
-app.use('/', index);
-app.use('/register', register);
+//Routes
+app.use('/', index)
+app.use('/register', register)
 
 // catch 404 and forward to error handler
-//
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
-//
 app.use(function(err, req, res, next) {
-	//console.log(err);
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-	//
-  res.status(err.status || 500);
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,7 @@
 var 	express = require('express'),
 		connect = require('../config/database.js'),
 		session = require('express-session'),
-		router = express.Router();
+		router = express.Router()
 
 router.post('/', function(req, res) {
 	var	login = req.body.login,
@@ -10,20 +10,23 @@ router.post('/', function(req, res) {
 		email = req.body.email,
 		gender = req.body.gender,
 		city = req.body.city,
-		age = req.body.age;
-	if (login && name && lastname && email && age && gender && city) {
-		//connect.query("SELECT * FROM user WHERE login = ? OR email = ?", [login, email], (err, rows, result) => {
-		//if (err) console.log(err);
+		age = req.body.age,
+		pswd = req.body.pswd,
+		repswd = req.body.repswd
+	if (login && name && lastname && email && age && gender && city && pswd && repswd) {
+		connect.query("SELECT * FROM user WHERE login = ? OR email = ?", [login, email], (err, rows, result) => {
+			if (err) console.log(err)
 		if (login.length > 60 || email.length > 150 || lastname.length > 60 || name.length > 60) {
-			req.session.error('trop long !');
-			res.redirect('/');
+			req.session.error('trop long !')
+			res.redirect('/')
 		} else {
-			res.send("ON EST LA !");
+			res.send("ON EST LA !")
 		}
 	} else {
-			req.session.error('Veuillez remplir tous les champs.');
-			res.redirect('/');
+		console.log('test')
+		req.session.error('Veuillez remplir tous les champs.')
+		res.redirect('/')
 	}
-});
+})
 
-module.exports = router;
+module.exports = router
