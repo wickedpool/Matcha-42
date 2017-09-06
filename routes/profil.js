80f1a4fb5778ad4e70d3f8c0ca8d65f7767b0b7c
Wickedpool <thgiraud@student.42.fr>
2017-09-06 18:15:18 +0200

    register done

--- routes/profil.js
+++ routes/profil.js
@@ -1,8 +1,12 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/profil', function(req, res, next) {
+router.get('/', function(req, res, next) {
 	res.render('profil', { title: 'Express' })
+})
+
+router.post('/', function(req, res) {
+	
 })
 
 module.exports = router
