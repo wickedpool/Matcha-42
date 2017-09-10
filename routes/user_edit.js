e55843b6dad47ca25e2a267ecfc63477c0965dce
Wickedpool <thgiraud@student.42.fr>
2017-09-10 22:03:29 +0200

    alltags need to be del

--- routes/user_edit.js
+++ routes/user_edit.js
@@ -31,4 +31,26 @@ router.post('/', function(req, res, next) {
 	}
 })
 
+router.get('/user_edit/del/:tag', function(req, res) {
+	if (req.session && req.session.login) {
+		connection.query('SELECT COUNT(*) AS count FROM tag WHERE login = ? AND tag = ?', [req.session.login, req.params.tag], (err, result) => {
+            if (err) console.log(err)
+            if (result[0] && result[0].count != 0) {
+                connection.query('DELETE FROM tag WHERE login = ? AND tag = ?', [req.session.login, req.params.tag], (err) => {
+                    if (err) console.log(err)
+        			req.session.success = "Votre tag a bien ete supprime"
+                    res.redirect('/user_edit')
+                })
+            }
+        })
+    } else if (req.session && req.session.user) {
+        req.session.error = "Vous devez renseigner un tag."
+        res.redirect('/user_edit')
+    } else {
+        req.session.error = "Vous devez être connecté pour accéder a cette page."
+        res.redirect('/register')
+		
+	}
+})
+
 module.exports = router
