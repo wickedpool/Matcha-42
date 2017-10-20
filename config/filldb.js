b36d0eeb6acc11d306d72a156158be6a131dde90
Wickedpool <thgiraud@student.42.fr>
2017-10-20 19:11:18 +0200

    doooone

--- config/filldb.js
+++ config/filldb.js
@@ -35,7 +35,7 @@ console.log('Database changed !');
 //Filling database
 
 //		USERS
-connection.query('CREATE TABLE IF NOT EXISTS user (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, login VARCHAR(100) NOT NULL, name VARCHAR(100) NOT NULL, lastname VARCHAR(100) NOT NULL, email VARCHAR(255) NOT NULL, passwd VARCHAR(255) NOT NULL, register DATETIME, age INT(3), sexe VARCHAR(25), city VARCHAR(255), description VARCHAR(10000), interest VARCHAR(255), mainpic VARCHAR(255), pic1 VARCHAR(255), pic2 VARCHAR(255), pic3 VARCHAR(255), pic4 VARCHAR(255), online BOOLEAN DEFAULT FALSE, latitude FLOAT, longitude FLOAT, hash VARCHAR(255))', function(err) {
+connection.query('CREATE TABLE IF NOT EXISTS user (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, login VARCHAR(100) NOT NULL, name VARCHAR(100) NOT NULL, lastname VARCHAR(100) NOT NULL, email VARCHAR(255) NOT NULL, passwd VARCHAR(255) NOT NULL, register DATETIME, age INT(3), sexe VARCHAR(25), city VARCHAR(255), description VARCHAR(10000), interest VARCHAR(255), mainpic VARCHAR(255), pic1 VARCHAR(255), pic2 VARCHAR(255), pic3 VARCHAR(255), pic4 VARCHAR(255), online BOOLEAN DEFAULT FALSE, latitude FLOAT, longitude FLOAT, hash VARCHAR(255), connect DATETIME)', function(err) {
 	if (err) throw err;
 	else {
 		console.log('Table user created !');
@@ -75,7 +75,7 @@ connection.query('CREATE TABLE IF NOT EXISTS matched (id INT(9) UNSIGNED AUTO_IN
 })
 
 //		NOTIFICATIONS
-connection.query('CREATE TABLE IF NOT EXISTS notif (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, login VARCHAR(100) NOT NULL, sendat DATETIME, type VARCHAR(20), msg VARCHAR(50))', function(err) {
+connection.query('CREATE TABLE IF NOT EXISTS notif (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, login VARCHAR(100) NOT NULL, sendat DATETIME, type VARCHAR(20), msg VARCHAR(50), readed INT(1))', function(err) {
 	if (err) throw err;
 	else {
 		console.log('Table notif created !');
